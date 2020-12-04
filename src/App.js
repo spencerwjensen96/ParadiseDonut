198b04732598979702de7cea70c5a866383dd70e
wlggraham <56554858+wlggraham@users.noreply.github.com>
2020-12-03 17:27:30 -0700

    try 1: attach to api endpoint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  window.onload = function () {
+    const createCORSRequest = function (method, url) {
+        const xhr = new XMLHttpRequest();
+        if (`withCredentials` in xhr) {
+            // Most browsers.
+            xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != `undefined`) {
+            // IE8 & IE9
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        } else {
+            // CORS not supported.
+            xhr = null;
+        }
+        return xhr;
+    };
+    const method = `GET`;
+    const url = `https://61eq041wd8.execute-api.us-east-1.amazonaws.com/beta/DonutDBFunction`;
+    const request = createCORSRequest(method, url);
+    request.setRequestHeader(`Content-Type`, `application/json; charset=UTF-8`);
+    request.send();
+
+    request.onload = function () {
+        const content = request.responseText;
+        console.log(content);
+    };
+
+    request.onerror = function () {
+        const error = request.responseText;
+        console.log(error);
+    };
+};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +51,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{content}</p>
       </header>
     </div>
   );

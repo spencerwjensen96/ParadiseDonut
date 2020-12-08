1de9f58a015e2159b0c9a86bcbb86f98b1451999
masonwells <masonbrockbank@gmail.com>
2020-12-08 13:24:37 -0700

    CSS updates getting closer to the end

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     return () => mounted = false;
   }, [])
 
-  
+
 
 
 
@@ -51,62 +51,18 @@ function App() {
               </div>
             </div>
             {/* !PAGE CONTENT! */}
-            <div className="w3-main w3-content w3-padding" style={{maxWidth: '1200px', marginTop: '100px'}}>
+            <div className="w3-main w3-content w3-padding" style={{maxWidth: '80%', marginTop: '100px'}}>
               ​
-              {/* First Photo Grid*/}
-              <div className="w3-row-padding w3-padding-16 w3-center" id="donut">
-                
-                
-                <div className="w3-quarter">
-                  <img src="https://is531-paradisedonut-finalproject.s3.amazonaws.com/FinalMedia/d%231.png" alt="Sandwich" style={{width: '100%'}} />
-                   <ul>Glazed :  {list.map(item => <li key={item.PK}> <img src={item.URL}  style={{width: '100%'}} /></li>)}</ul>
-                  <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                  
+        
+                <div>   
+                      <ul className="flex-container">
+                          {list.map(item => <img key={item.PK} src={item.URL} />)}
+                      </ul>
                 </div>
 
-       
-
-                <div className="w3-quarter">
-                  <img src="/w3images/steak.jpg" alt="Steak" style={{width: '100%'}} />
-                  <h3>Let Me Tell You About This Steak</h3>
-                  <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                </div>
-                <div className="w3-quarter">
-                  <img src="/w3images/cherries.jpg" alt="Cherries" style={{width: '100%'}} />
-                  <h3>Cherries, interrupted</h3>
-                  <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                  <p>What else?</p>
-                </div>
-                <div className="w3-quarter">
-                  <img src="/w3images/wine.jpg" alt="Pasta and Wine" style={{width: '100%'}} />
-                  <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
-                  <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                </div>
-              </div>
-              {/* Second Photo Grid*/}
-              <div className="w3-row-padding w3-padding-16 w3-center">
-                <div className="w3-quarter">
-                  <img src="/w3images/popsicle.jpg" alt="Popsicle" style={{width: '100%'}} />
-                  <h3>All I Need Is a Popsicle</h3>
-                  <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                </div>
-                <div className="w3-quarter">
-                  <img src="/w3images/salmon.jpg" alt="Salmon" style={{width: '100%'}} />
-                  <h3>Salmon For Your Skin</h3>
-                  <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                </div>
-                <div className="w3-quarter">
-                  <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{width: '100%'}} />
-                  <h3>The Perfect Sandwich, A Real Classic</h3>
-                  <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                </div>
-                <div className="w3-quarter">
-                  <img src="/w3images/croissant.jpg" alt="Croissant" style={{width: '100%'}} />
-                  <h3>Le French</h3>
-                  <p>Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-                </div>
-              </div>
-              ​
+              <br></br>
+              <br></br>
+           ​
               {/* Pagination */}
               <div className="w3-center w3-padding-32">
                 <div className="w3-bar">
@@ -118,12 +74,14 @@ function App() {
                   <a href="#" className="w3-bar-item w3-button w3-hover-black">»</a>
                 </div>
               </div>
+
+
+
               <hr id="about" />
-              ​
               {/* About Section */}
               <div className="w3-container w3-padding-32 w3-center">  
-                <h3>Our story - nothing but donuts</h3><br />
-                <img src="/w3images/chef.jpg" alt="Me" className="w3-image" style={{display: 'block', margin: 'auto'}} width={800} height={533} />
+                <h3><b>Our Story</b></h3><br />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Dunkin%27_Donuts%2C_W_Irlo_Bronson_Memorial_Highway%2C_Osceola_County.jpg/1200px-Dunkin%27_Donuts%2C_W_Irlo_Bronson_Memorial_Highway%2C_Osceola_County.jpg" alt="Me" className="w3-image" style={{display: 'block', margin: 'auto'}} width={800} height={533} />
                 <div className="w3-padding-32">
                   <h4><b>I am Who I Am!</b></h4>
                   <h6><i>With Passion For Real, Good Donuts!</i></h6>
@@ -133,37 +91,27 @@ function App() {
               <hr />
               {/* Footer */}
               <footer className="w3-row-padding w3-padding-32">
-                <div className="w3-third">
+                <div className="w3-half">
                   <h3>I LOVE DONUTS</h3>
                   <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
                 </div>
-                <div className="w3-third">
+                <div className="w3-half">
                   <h3>DONUT POSTS</h3>
                   <ul className="w3-ul w3-hoverable">
                     <li className="w3-padding-16">
-                      <img src="/w3images/workshop.jpg" className="w3-left w3-margin-right" style={{width: '50px'}} />
+                      <img src="https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_396,q_75,w_704/https://assets.simpleviewinc.com/simpleview/image/upload/crm/lafayette/Cajun-Market-Donut-69ff83cf5056b3a_69ff851a-5056-b3a8-49b98942484ff041.jpg" className="w3-left w3-margin-right" style={{width: '100px'}} />
                       <span className="w3-large">Lorem</span><br />
                       <span>Sed mattis nunc</span>
                     </li>
                     <li className="w3-padding-16">
-                      <img src="/w3images/gondol.jpg" className="w3-left w3-margin-right" style={{width: '50px'}} />
-                      <span className="w3-large">Ipsum</span><br />
-                      <span>Praes tinci sed</span>
-                    </li> 
+                      <img src="https://www.cookingchanneltv.com/content/dam/images/cook/video/7/75/756/7566/7566919.jpg" className="w3-left w3-margin-right" style={{width: '100px'}} />
+                      <span className="w3-large">Lorem</span><br />
+                      <span>Sed mattis nunc</span>
+                    </li>
                   </ul>
                 </div>
                 ​
-                <div className="w3-third w3-serif">
-                  <h3>POPULAR TAGS
-                  </h3>
-                  <p>
-                    <span className="w3-tag w3-black w3-margin-bottom">Donut</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">New York</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dinner</span>
-                    <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Original</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">France</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Drinks</span>
-                    <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Holy donut!</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Flavors</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Cuisine</span>
-                    <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Donut Gods</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dressing</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fried</span>
-                    <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">So many Donuts</span> <span className="w3-tag w3-dark-grey w3-small w3-margin-bottom">Duck</span>
-                  </p>
-                </div>
+               
               </footer>
               ​
               {/* End page content */}
